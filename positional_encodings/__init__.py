caa51c06714f870f410db3260fae347e69997cc5
Chin Yun Yu <ya70201@gmail.com>
2021-07-28 14:39:44 +0800

    fix import error

--- positional_encodings/__init__.py
+++ positional_encodings/__init__.py
@@ -1,2 +1,2 @@
-from positional_encodings.positional_encodings import PositionalEncoding1D, PositionalEncoding2D, PositionalEncoding3D
-from positional_encodings.positional_encodings import PositionalEncodingPermute1D, PositionalEncodingPermute2D, PositionalEncodingPermute3D
+from .positional_encodings import PositionalEncoding1D, PositionalEncoding2D, PositionalEncoding3D
+from .positional_encodings import PositionalEncodingPermute1D, PositionalEncodingPermute2D, PositionalEncodingPermute3D
